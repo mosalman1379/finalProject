6a3b29f77426f78ac6c9f728fcaf897903f61f25
mohammad <mosalman1379@gmail.com>
2021-07-29 20:20:54 +0430

    final commit in phase 1

--- products/forms.py
+++ products/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from products.models import Product
 
-
+# product form
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
@@ -11,13 +11,13 @@ class ProductForm(forms.ModelForm):
         widgets = {
             'taxable': forms.CheckboxInput,
         }
-
+    # checking validation of image file
     def clean_catalog_image(self):
         catalog_image = self['catalog_image'].data.name
         if catalog_image.endswith('.jpg') or catalog_image.endswith('.jpeg'):
             return catalog_image
         raise forms.ValidationError('invalid file extension(only jpg or jpeg)')
-
+    # chcecking validation of pdf file
     def clean_catalog_pdf(self):
         catalog_pdf=self['catalog_pdf'].data.name
         if catalog_pdf.endswith('.pdf'):
